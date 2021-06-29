053d4e9fc306bddf03b7e08b5f3af25b0955c690
AotuMan <460373604@qq.com>
2021-06-29 16:23:20 +0800

    feat: add

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,280 +1,240 @@
 const blog = [
-    {
-        title: 'JS',
-        collapsable: true,
-        children: [
-            'js/手写系列',
-            "js/promise",
-            'js/运算符',
-            'js/预编译',
-            'js/clone',
-            'js/执行过程',
-            'js/箭头函数',
-            'js/闭包',
-            "js/元编程",
-            "js/原型",
-            "js/迭代器",
-            "js/EventLoop",
-            "js/0.1+0.2"
-        ],
-
-    },
-    {
-        title: 'HTTP',
-        collapsable: true,
-        children: [
-            'http/HTTP协议1',
-            'http/HTTP缓存',
-            'http/DNS',
-            'http/TCP-IP'
-        ]
-    },
-    {
-        title: 'Node',
-        collapsable: true,
-        children: [
-            'Node/自动加载全局路由',
-            'Node/全局异常处理',
-            'Node/使用Sequelize操作mySql',
-            'Node/集成jwt',
-            'Node/SSR',
-            'Node/BFF架构初探',
-            'Node/MPA架构初探',
-            'Node/PM2'
-        ]
-    },
-    {
-        title: '前端工程化',
-        collapsable: true,
-        children: [
-            'engineering/打造属于自己的脚手架',
-            'engineering/脚手架的可视化操作',
-            'engineering/Jenkins安装',
-            'engineering/sonar简单使用',
-            'engineering/microbundle'
-        ]
-    },
-    {
-        title: '性能优化',
-        collapsable: true,
-        children: [
-            'optimization/网速检测',
-            'optimization/资源阻塞',
-            'optimization/渲染流程',
-            'optimization/指标',
-            'optimization/Chrome性能分析',
-            'optimization/资源优化',
-            'optimization/传输加载优化',
-            'optimization/性能优化启示录',
-            'optimization/Node性能调优',
-            'optimization/MPA性能优化小试',
-        ]
-    },
-    {
-        title: 'webpack',
-        collapsable: true,
-        children: [
-            'webpack/webpack基础',
-            'webpack/代码分割',
-            'webpack/webpack打包',
-            'webpack/源码解析一',
-            'webpack/源码解析二',
-            'webpack/源码解析三',
-            'webpack/源码解析四',
-            'webpack/源码解析五',
-            'webpack/源码解析六',
-            'webpack/源码分析总结',
-            'webpack/手写Webpack',
-            'webpack/webpack优化',
-            'webpack/webpack5'
-        ]
-    },
-    {
-        title: 'TS',
-        collapsable: true,
-        children: [
-            "ts/基础",
-            "ts/进阶",
-            "ts/类与接口",
-            "ts/装饰器",
-            "ts/装饰器的使用"
-        ]
-    },
-    {
-        title: 'Framework',
-        collapsable: true,
-        children: [
-            "framework/redux",
-            "framework/react-redux",
-            "framework/react-router",
-            "framework/hooks-redux",
-            "framework/koa"
-        ]
-    },
-    {
-        title: '前端安全',
-        collapsable: true,
-        children: [
-            "safety/encryption",
-            "safety/safe"
-        ]
-    },
-    {
-        title: 'Nginx',
-        collapsable: true
-    },
-    {
-        title: '微前端',
-        collapsable: true
-    },
-    {
-        title: '小程序原理',
-        collapsable: true
-    },
-    {
-        title: '杂谈',
-        collapsable: true,
-        children: [
-            'talk/book'
-        ]
-    }
-]
+  {
+    title: "JS",
+    collapsable: true,
+    children: [
+      "js/手写系列",
+      "js/promise",
+      "js/运算符",
+      "js/预编译",
+      "js/clone",
+      "js/执行过程",
+      "js/箭头函数",
+      "js/闭包",
+      "js/元编程",
+      "js/原型",
+      "js/迭代器",
+      "js/EventLoop",
+      "js/0.1+0.2",
+    ],
+  },
+  {
+    title: "HTTP",
+    collapsable: true,
+    children: ["http/HTTP协议1", "http/HTTP缓存", "http/DNS", "http/TCP-IP"],
+  },
+  {
+    title: "Node",
+    collapsable: true,
+    children: [
+      "Node/自动加载全局路由",
+      "Node/全局异常处理",
+      "Node/使用Sequelize操作mySql",
+      "Node/集成jwt",
+      "Node/SSR",
+      "Node/BFF架构初探",
+      "Node/MPA架构初探",
+      "Node/PM2",
+    ],
+  },
+  {
+    title: "前端工程化",
+    collapsable: true,
+    children: [
+      "engineering/打造属于自己的脚手架",
+      "engineering/脚手架的可视化操作",
+      "engineering/Jenkins安装",
+      "engineering/sonar简单使用",
+      "engineering/microbundle",
+    ],
+  },
+  {
+    title: "性能优化",
+    collapsable: true,
+    children: [
+      "optimization/网速检测",
+      "optimization/资源阻塞",
+      "optimization/渲染流程",
+      "optimization/指标",
+      "optimization/Chrome性能分析",
+      "optimization/资源优化",
+      "optimization/传输加载优化",
+      "optimization/性能优化启示录",
+      "optimization/Node性能调优",
+      "optimization/MPA性能优化小试",
+    ],
+  },
+  {
+    title: "webpack",
+    collapsable: true,
+    children: [
+      "webpack/webpack基础",
+      "webpack/代码分割",
+      "webpack/webpack打包",
+      "webpack/源码解析一",
+      "webpack/源码解析二",
+      "webpack/源码解析三",
+      "webpack/源码解析四",
+      "webpack/源码解析五",
+      "webpack/源码解析六",
+      "webpack/源码分析总结",
+      "webpack/手写Webpack",
+      "webpack/webpack优化",
+      "webpack/webpack5",
+    ],
+  },
+  {
+    title: "TS",
+    collapsable: true,
+    children: [
+      "ts/基础",
+      "ts/进阶",
+      "ts/类与接口",
+      "ts/装饰器",
+      "ts/装饰器的使用",
+    ],
+  },
+  {
+    title: "Framework",
+    collapsable: true,
+    children: [
+      "framework/redux",
+      "framework/react-redux",
+      "framework/react-router",
+      "framework/hooks-redux",
+      "framework/koa",
+    ],
+  },
+  {
+    title: "前端安全",
+    collapsable: true,
+    children: ["safety/encryption", "safety/safe"],
+  },
+  {
+    title: "Nginx",
+    collapsable: true,
+  },
+  {
+    title: "微前端",
+    collapsable: true,
+  },
+  {
+    title: "小程序原理",
+    collapsable: true,
+  },
+  {
+    title: "杂谈",
+    collapsable: true,
+    children: ["talk/book"],
+  },
+];
 
 // 算法
 const arithmetic = [
-    {
-        titie: '原理',
-        collapsable: true,
-        children: [
-            'principle/skill'
-        ]
-    },
-    {
-        title: '递归',
-        collapsable: true,
-        children: [
-            'recursion/递归'
-        ]
-    },
-    {
-        title: '栈',
-        collapsable: true,
-        children: [
-            'stack/栈',
-            'stack/进制转换',
-            'stack/有效的括号'
-        ]
-    },
-    {
-        title: '队列',
-        collapsable: true,
-        children: [
-            "queue/队列",
-            "queue/练习"
-        ]
-    },
-    {
-        title: '链表',
-        collapsable: true,
-        children: [
-            'linkedList/链表',
-            'linkedList/套路'
-        ]
-    },
-    {
-        title: '字典和散列表',
-        collapsable: true,
-        children: [
-            'dictionary/字典和散列表'
-        ]
-    },
-    {
-        title: '集合',
-        collapsable: true,
-        children: [
-            'set/集合'
-        ]
-    },
-    {
-        title: '排序',
-        collapsable: true,
-        children: [
-            "sort/排序和搜索"
-        ]
-    },
-
-]
-
+  {
+    titie: "原理",
+    collapsable: true,
+    children: ["principle/skill"],
+  },
+  {
+    title: "递归",
+    collapsable: true,
+    children: ["recursion/递归"],
+  },
+  {
+    title: "栈",
+    collapsable: true,
+    children: ["stack/栈", "stack/进制转换", "stack/有效的括号"],
+  },
+  {
+    title: "队列",
+    collapsable: true,
+    children: ["queue/队列", "queue/练习"],
+  },
+  {
+    title: "链表",
+    collapsable: true,
+    children: ["linkedList/链表", "linkedList/套路"],
+  },
+  {
+    title: "树",
+    collapsable: true,
+    children: ["tree/树","tree/解题套路"],
+  },
+  {
+    title: "字典和散列表",
+    collapsable: true,
+    children: ["dictionary/字典和散列表"],
+  },
+  {
+    title: "集合",
+    collapsable: true,
+    children: ["set/集合"],
+  },
+  {
+    title: "排序",
+    collapsable: true,
+    children: ["sort/排序和搜索"],
+  },
+];
 
 const react = [
-    {
-        title: '基础',
-        collapsable: true,
-        children: [
-            'API',
-            'ReactElement',
-            'JSX',
-            'ReactChildren',
-            "react-fiber",
-            "requestAnimationFrame",
-            "requestIdleCallback",
-        ]
-    },
-    {
-        title: '创建更新',
-        collapsable: true,
-        children: [
-            "Render",
-            "update",
-            "Fiber",
-            "expirationTime",
-            "updateQueue"
-        ]
-    },
-    {
-        title: '任务调度',
-        collapsable: true,
-        children: [
-            'scheduleWork',
-            '双缓存fiber树',
-            'workLoopSync',
-            'beginWork',
-            'HostComponent',
-            'ClassComponent',
-            'completeUnitOfWork',
-            'DOM创建',
-            'finishSyncRender'
-        ]
-    },
-    {
-        title: '功能',
-        collapsable: true,
-        children: [
-            "单节点Diff",
-            "多节点Diff",
-            "Context",
-            "Ref",
-            "memo",
-            "setState",
-            "合成事件",
-            "SchedulerHostConfig.default",
-        ]
-    },
-    {
-        title: 'Hooks',
-        collapsable: true,
-        children: [
-            "useMemo",
-            "useCallback",
-            "useEffect",
-            "useState"
-        ]
-    }
-]
-
+  {
+    title: "基础",
+    collapsable: true,
+    children: [
+      "API",
+      "ReactElement",
+      "JSX",
+      "ReactChildren",
+      "react-fiber",
+      "requestAnimationFrame",
+      "requestIdleCallback",
+    ],
+  },
+  {
+    title: "创建更新",
+    collapsable: true,
+    children: ["Render", "update", "Fiber", "expirationTime", "updateQueue"],
+  },
+  {
+    title: "任务调度",
+    collapsable: true,
+    children: [
+      "scheduleWork",
+      "双缓存fiber树",
+      "workLoopSync",
+      "beginWork",
+      "HostComponent",
+      "ClassComponent",
+      "completeUnitOfWork",
+      "DOM创建",
+      "finishSyncRender",
+    ],
+  },
+  {
+    title: "功能",
+    collapsable: true,
+    children: [
+      "单节点Diff",
+      "多节点Diff",
+      "Context",
+      "Ref",
+      "memo",
+      "setState",
+      "合成事件",
+      "SchedulerHostConfig.default",
+    ],
+  },
+  {
+    title: "Hooks",
+    collapsable: true,
+    children: ["useMemo", "useCallback", "useEffect", "useState"],
+  },
+];
 
 module.exports = {
-    '/blog/': blog,
-    '/arithmetic/': arithmetic,
-    '/react/': react
-}
+  "/blog/": blog,
+  "/arithmetic/": arithmetic,
+  "/react/": react,
+};
